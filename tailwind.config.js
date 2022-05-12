432bcff51fdae052c60a93e144dec56937867d96
pixiepucker <autumnoffic@gmail.com>
2022-05-12 17:57:41 -0500

    header and hero done

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./src/*.html"],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    colors: {
+      'maxyellow': '#FFCA61',
+      'lightcyan': '#D6FFFE',
+      'springgreen': '#1DE588',
+      'spacecadet': '#151335',
+      'midnightblue': '#2D216F',
+      'rosebonbon': '#FF399F',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
